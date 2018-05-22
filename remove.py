f8c887501651649714f6ba0e55b488edeaa7114c
Punit Vara <punit.vara@intel.com>
2018-05-22 15:56:36 +0530

    able to replace one liner zassert

--- remove.py
+++ remove.py
@@ -1,20 +1,23 @@
 import re,os,shutil
 
 def finddir():
-    for path, dirs, files in os.walk('./tests/posix/mutex'):  
+    for path, dirs, files in os.walk('./tests/posix/pthread'):  
             for names in files:
                 if names.endswith('.c'):
                     #for line in open(os.path.join(path, names), "rw"):
                     with open(os.path.join(path, names), 'r') as f:
-                        f.seek(0, 0)
-                        read_data = f.read()
-                        for line in open(os.path.join(path, names), 'r+'):
-                            fout = open(os.path.join(path, names)).read()
+                        lines = f.readlines()
+                        f.close()
+                        f = open(os.path.join(path, names), 'w')
+                        for line in lines:
                             if re.findall("zassert_", line):
-                                xyz = re.search(r'\\n\"\)\;', line)
-                                if xyz:
-                                    replace = re.sub(r'\\n\"\)\;', r'\"\)\;', line)
-                                    line = line.restrip(replace)
+                                    xyz = re.search(r'\\n\"\)\;', line)
+                                    if xyz:
+                                        replace = re.sub(r'\\n\"\)\;', r'");', line)
+                                        f.write(replace)
+                            else:
+                                f.write(line)
+                        f.close()
 
 def main():
     print("Hellow wor")
